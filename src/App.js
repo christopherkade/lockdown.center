1787a27d82432afbc34f92a23e252222814c80e4
Christopher Kade <c.kade96@gmail.com>
2020-03-17 16:01:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1&controls=0"></iframe>
+  );
+}
+
+export default App;
