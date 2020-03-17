e50066914ec487fa801740358b8336f5d5fc75d9
Christopher Kade <c.kade96@gmail.com>
2020-03-17 16:15:31 +0100

    Fix autoplay

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <iframe title="rickroll" width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1&controls=0"></iframe>
+    <iframe title="rickroll" width="100%" height="100%" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1&controls=0" frameBorder="0" allow="autoplay"></iframe>
   );
 }
 
