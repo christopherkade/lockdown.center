11ada76ec207e4064b82044c912453b03e8fdc3a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-26 08:18:22 +0000

    Deploy lockdown.center to github.com/christopherkade/lockdown.center.git:gh-pages

---
+++ precache-manifest.d0088a793dec71943b4ee7f7a7dac069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1591de0b920ec64efc5391e0e5c67642",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac399ba6175e3ecca8b2",
+    "url": "/static/css/main.8f543658.chunk.css"
+  },
+  {
+    "revision": "b2856a89cf35bfdba5df",
+    "url": "/static/js/2.701801f9.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.701801f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac399ba6175e3ecca8b2",
+    "url": "/static/js/main.a2006065.chunk.js"
+  },
+  {
+    "revision": "19a1fdd3ffdcf88433ef",
+    "url": "/static/js/runtime-main.3afcd878.js"
+  },
+  {
+    "revision": "b1212363af28dc58944a1a725c627732",
+    "url": "/static/media/sitting-reading.b1212363.svg"
+  }
+]);
